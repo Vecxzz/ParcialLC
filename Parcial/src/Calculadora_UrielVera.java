d5de9ba3bf284344abb38bfa21f0811d8457108d
Uriel Vera <112576496+Vecxzz@users.noreply.github.com>
2023-09-28 09:36:38 -0300

    Update Calculadora_UrielVera.java

--- Parcial/src/Calculadora_UrielVera.java
+++ Parcial/src/Calculadora_UrielVera.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Calculadora_UrielVera {
     public static void main(String[] args) {
         //Crear Scanner
         Scanner sc = new Scanner(System.in);
